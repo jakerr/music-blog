a6bf4a2a1ab6e92b0b0c47713830b705595136d6
Jake Kerr <kodafox@gmail.com>
2023-01-13 01:38:42 +0900

    Basic piano drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,113 @@
-import React from 'react';
+import React, {FC} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type NaturalNote = 'C' | 'D' | 'E' | 'F' | 'G' | 'A' | 'B'
+type Accidental = '#' | 'b' | undefined 
+type Note = {
+  name: NaturalNote
+  acc?: Accidental
+  oct: number
+}
+const CHROMA = "C_D_EF_G_A_B";
+
+const accidentalOffset = (acc: Accidental): number => {
+  if (acc == "#") {
+    return 1;
+  } else if (acc == "b") {
+    return -1;
+  }
+  return 0;
+}
+
+const noteIndex = (note: Note): number => {
+  const offset = accidentalOffset(note.acc);
+  const noteOrder = (CHROMA.indexOf(note.name) + offset);
+  return note.oct * 12 + noteOrder;
+}
+
+const noteForIndex = (idx: number): Note => {
+  const oct = Math.floor(idx / 12);
+  const chromaticDegree = idx % 12;
+  var noteName = CHROMA[chromaticDegree];
+  var acc = undefined;
+  if (noteName === "_") {
+    acc = "#"
+    noteName = CHROMA[chromaticDegree - 1];
+  }
+  return {
+    name: noteName as NaturalNote,
+    acc: acc as Accidental,
+    oct: oct,
+  };
+}
+
+const noteColor = (note: Note): string => {
+  const offset = accidentalOffset(note.acc);
+  return offset === 0 ? "white" : "black";
+}
+
+const Key: FC<{
+  note: Note
+}> = ({note}) => {
+  const color = noteColor(note);
+  return (
+    <div className={`key ${color}`}>
+    </div>
+  )
+}
+
+const Keys: FC<{
+  from: Note,
+  to: Note
+}> = ({from, to}) => {
+  const fromIndex = noteIndex(from);
+  const toIndex = noteIndex(to);
+  const fromNote2 = noteForIndex(fromIndex);
+  const toNote2 = noteForIndex(toIndex);
+  const allNoteIds = Array(toIndex - fromIndex).fill(0).map((i, idx) => i + idx + fromIndex);
+  const allNotes = allNoteIds.map((noteId) => noteForIndex(noteId));
+  console.log(`All notes ${allNoteIds}`);
+  console.log(`All notes ${allNotes}`);
+  return (
+    <>
+    <div className='key-board'>
+      {
+      allNotes.map((note) => (
+        <Key key={noteIndex(note)} note={note}></Key>
+      ))
+      }
+    </div>
+
+      {/* Notes from: {from.name}{from.acc}{from.oct} to: {to.name}{to.acc}{to.oct}
+      ({fromIndex}) ~ ({toIndex})
+      <br>
+      </br>
+      Notes round trip from: {fromNote2.name}{fromNote2.acc}{fromNote2.oct} to: {toNote2.name}{toNote2.acc}{toNote2.oct} */}
+    </>
+  )
+}
+
 function App() {
+  const from: Note = {
+    name: "C",
+    oct: 0,
+  }
+  const to: Note = {
+    name: "C",
+    oct: 3,
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is a typical piano keyboard with three octaves starting on C and ending on the C three octaves higher.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Keys from={from} to={to}></Keys>
+        <p>
+          The white notes are called "natural" notes and they have the names "C, D, E, F, G, A, B" repeating up the keyboard.
+        </p>
       </header>
     </div>
   );

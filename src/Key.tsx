207989d0c48392c0d24226cc5992fab006a28c96
Jake Kerr <kodafox@gmail.com>
2023-02-07 16:30:47 +0900

    Add note names to keys

--- src/Key.tsx
+++ src/Key.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Note, noteColor, bracketClass } from "./Notes";
+import { Note, noteColor, bracketClass, prettyNoteName } from "./Notes";
 
 export const Key: FC<{
   note: Note;
@@ -9,11 +9,13 @@ export const Key: FC<{
   const bracketCss = bracketClass(note.bracket);
   const bracketColor = note.bracketColor;
   const noteCss = `note-${note.name}${note.acc ? "s" : ""}`;
+  const prettyName = prettyNoteName(note);
   return (
     <div className={`key ${blackWhite} ${hl} ${noteCss}`}>
       <div className={`key-bracket ${bracketCss} ${bracketColor}`}>
         <div className={`key-bracket-label`}>{note.bracketLabel}</div>
       </div>
+      <span className="note-label">{prettyName}</span>
     </div>
   );
 };
